b3f88e095edd19d01a165c24665934aa94a97f9f
高天宇 <tianyu8125@163.com>
2016-04-15 16:26:56 +0800

    update 4 news and notices

--- StuSite/StuSiteMVCModels/News.cs
+++ StuSite/StuSiteMVCModels/News.cs
@@ -13,8 +13,8 @@ namespace StuSiteMVC.Models
         public int id { get; set; }                    //新闻ID
         public string NewsTittle { get; set; }         //新闻标题
         public string NewsMain { get; set; }           //新闻主题
-        public DateTime? NewsDate { get; set; }         //发布日期
-        public DateTime? NewsDatetime { get; set; }     //发布日期+时间
+        public DateTime? NewsDate { get; set; }        //发布日期
+        public DateTime? NewsDatetime { get; set; }    //发布日期+时间
         public Admin NewsPublisher { get; set; }       //发布者
         public NState NState { get; set; }             //新闻状态
     }
