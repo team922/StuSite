02d747e90dfa37e9ba5f479148aa76d1f07415c1
高天宇 <tianyu8125@163.com>
2016-05-27 09:30:41 +0800

    new StateManager

---
+++ StuSite/StuSiteMVCBLL/StateManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StuSiteMVC.DAL;
+using StuSiteMVC.Models;
+
+namespace StuSiteMVC.BLL
+{
+    public class StateManager
+    {
+        public NState GetNStateById(int id)
+        {
+            return new NStateService().GetNStateById(id);
+        }
+    }
+}
