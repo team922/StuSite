8b8d594ea01538e7886ef01b5c049b84a9007c39
高天宇 <tianyu8125@163.com>
2016-05-27 10:32:51 +0800

    update addnews

---
+++ StuSite/StuSiteMVC/Javascript/htmlcode.js
@@ -0,0 +1,10 @@
+﻿function htmlencode(string) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(string));
+    return div.innerHTML;
+}
+function htmldecode(strings) {
+    var div = document.createElement('div');
+    div.innerHTML = string;
+    return div.innerText || div.textContent;
+}
