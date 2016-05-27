8e8849d8b74af1ef6e0b61a6806f22ff0119d180
高天宇 <tianyu8125@163.com>
2016-05-27 09:34:41 +0800

    admin frame add news

--- StuSite/StuSiteMVC/Controllers/AdminController.cs
+++ StuSite/StuSiteMVC/Controllers/AdminController.cs
@@ -30,5 +30,90 @@ namespace StuSiteMVC.Controllers
                 return View("../Account/Admin");
             }
         }
+
+        public ActionResult LoginChack()
+        {
+            if (Session["Admin"] == null)
+            {
+                return Content("false");
+            }
+            else
+            {
+                return Content("true");
+            }
+        }
+
+        public ActionResult StuManage1()
+        {
+            return View();
+        }
+
+        public ActionResult StuManage2()
+        {
+            return View();
+        }
+
+        public ActionResult StuManage3()
+        {
+            return View();
+        }
+
+        public ActionResult TeaManage1()
+        {
+            return View();
+        }
+
+        public ActionResult TeaManage2()
+        {
+            return View();
+        }
+
+        public ActionResult NoticeManage1()
+        {
+            return View();
+        }
+
+        public ActionResult NoticeManage2()
+        {
+            return View();
+        }
+
+        public ActionResult NewsManage1()
+        {
+            return View();
+        }
+
+        public ActionResult NewsManage2()
+        {
+            return View();
+        }
+
+        public ActionResult AddNews()
+        {
+            string news_title = Request.Form["title"];//获取标题
+            string news_state = Request.Form["select_text"];//获取状态
+            string news_body = Server.HtmlEncode(Request.Form["ckeditor_html"]);//获取内容
+
+            Admin A = new Admin();
+            A = Session["Admin"] != null ? (Admin)Session["Admin"] : null;
+
+            News news = new News();
+            news.NewsTitle = news_title;
+            news.NewsMain = news_body;
+            news.NewsDate = new IPManager().GetDateTime();
+            news.NewsPublisher = A;
+            news.NState = new StateManager().GetNStateById(Convert.ToInt16(news_state));
+            news.NewsHits = 0;
+
+            if (new NewsManager().AddNews(news))
+            {
+                Response.Write("<script>alert('添加新闻成功！')</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('添加新闻失败！')</script>");
+            }
+            return View("NewsManage1");
+        }
     }
 }
