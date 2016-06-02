3966ed761573307529a4df2c4cad62130044bb54
高天宇 <tianyu8125@163.com>
2016-06-02 18:49:08 +0800

    update ckeditor config.js

--- StuSite/StuSiteMVC/CKeditor/config.js
+++ StuSite/StuSiteMVC/CKeditor/config.js
@@ -9,22 +9,21 @@ CKEDITOR.editorConfig = function( config ) {
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
-	config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];
+    config.toolbarGroups = [
+            { name: 'document', groups: ['mode', 'document', 'doctools'] },
+            { name: 'clipboard', groups: ['clipboard', 'undo'] },
+            { name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+            { name: 'links', groups: ['links'] },
+            { name: 'insert', groups: ['insert'] },
+            { name: 'forms', groups: ['forms'] },
+            { name: 'others', groups: ['others'] },
+            { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+            { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
+            { name: 'styles', groups: ['styles'] },
+            { name: 'tools', groups: ['tools'] },
+            { name: 'colors', groups: ['colors'] },
+            { name: 'about', groups: ['about'] }
+    ];
 	config.filebrowserUploadUrl = "/Admin/ImageUpload";
 
 	config.language = 'zh-cn';
@@ -35,10 +34,10 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	config.removeButtons = 'Superscript,About,Scayt,Anchor,RemoveFormat,Blockquote,Styles,PasteFromWord,Cut,Strike,Indent,Outdent,Subscript,SpecialChar';
 
 	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
+	config.format_tags = 'p;h1;h2;h3;h3;h4;h5;h6';
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
