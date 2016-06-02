46c5ca5f226d37dda64a8f372e3e8d1241c48ebe
高天宇 <tianyu8125@163.com>
2016-06-02 20:43:56 +0800

    update CKeditor

--- StuSite/StuSiteMVC/CKeditor/config.js
+++ StuSite/StuSiteMVC/CKeditor/config.js
@@ -3,42 +3,37 @@
  * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
-
-	// The toolbar groups arrangement, optimized for two toolbar rows.
+CKEDITOR.editorConfig = function (config) {
     config.toolbarGroups = [
-            { name: 'document', groups: ['mode', 'document', 'doctools'] },
-            { name: 'clipboard', groups: ['clipboard', 'undo'] },
-            { name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
-            { name: 'links', groups: ['links'] },
-            { name: 'insert', groups: ['insert'] },
-            { name: 'forms', groups: ['forms'] },
-            { name: 'others', groups: ['others'] },
-            { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
-            { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
-            { name: 'styles', groups: ['styles'] },
-            { name: 'tools', groups: ['tools'] },
-            { name: 'colors', groups: ['colors'] },
-            { name: 'about', groups: ['about'] }
+		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
+		{ name: 'clipboard', groups: ['clipboard', 'undo'] },
+		{ name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+		{ name: 'forms', groups: ['forms'] },
+		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
+		{ name: 'links', groups: ['links'] },
+		{ name: 'insert', groups: ['insert'] },
+		{ name: 'styles', groups: ['styles'] },
+		{ name: 'colors', groups: ['colors'] },
+		{ name: 'tools', groups: ['tools'] },
+		{ name: 'others', groups: ['others'] },
+		{ name: 'about', groups: ['about'] }
     ];
-	config.filebrowserUploadUrl = "/Admin/ImageUpload";
+    config.filebrowserUploadUrl = "/Admin/ImageUpload";
 
-	config.language = 'zh-cn';
-	config.width = 700;
-	config.height = 300;
-	config.toolbarCanCollapse = false;
-	config.resize_enabled = false;
+    config.language = 'zh-cn';
+    config.width = 700;
+    config.height = 300;
+    config.toolbarCanCollapse = false;
+    config.resize_enabled = false;
 
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Superscript,About,Scayt,Anchor,RemoveFormat,Blockquote,Styles,PasteFromWord,Cut,Strike,Indent,Outdent,Subscript,SpecialChar';
+    // Remove some buttons provided by the standard plugins, which are
+    // not needed in the Standard(s) toolbar.
+    config.removeButtons = 'NewPage,Templates,Cut,Copy,PasteFromWord,Find,Replace,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Strike,RemoveFormat,NumberedList,Outdent,Indent,BulletedList,Blockquote,BidiLtr,BidiRtl,Anchor,Flash,Smiley,SpecialChar,PageBreak,Iframe,Styles,ShowBlocks,Link,Unlink,Font,About,Language';
 
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;h3;h4;h5;h6';
+    // Set the most common block elements.
+    config.format_tags = 'p;h1;h2;h3;h3;h4;h5;h6';
 
-	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
+    // Simplify the dialog windows.
+    config.removeDialogTabs = 'image:advanced;link:advanced';
 };
